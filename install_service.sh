fa8623d538884131b91af90c5ffc7e8eea90981a
João Marcos <marcospb19@hotmail.com>
2020-05-16 19:27:30 -0300

    Adding aditional prompt to install_service.sh

--- install_service.sh
+++ install_service.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
-set -ex
+echo 'This script installs the service that launches betterlockscreen when the system sleeps'
+echo ''
+
+read -p 'Proceed? [Y/n] ' yn
+
+if ! ( [ "$yn" = '' ] || [ "$yn" = 'Y' ] || [ "$yn" = 'y' ] ); then
+	echo "Exiting."
+	exit 0
+fi
+
 
 if [[ "$USER" != 'root' ]]; then
 	echo "User is not root, exiting." >&2
 	exit 1
 fi
 
+set -ex
 cp system/betterlockscreen.service /usr/lib/systemd/system/
-
 sudo systemctl start  betterlockscreen.service
 sudo systemctl enable betterlockscreen.service
+set +ex
 
 echo "Done!"
